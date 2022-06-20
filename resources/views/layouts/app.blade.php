a29ece0ba6c4e1aa379d71b0fc7294a0b4bca474
kamyar Ghaffari <kamyar.ghaffari98@gmail.com>
2022-06-20 23:05:21 +0430

    change local and production links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,29 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        @if(config('app.env') == 'production')
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+            <!-- Fonts -->
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+            <!-- Styles -->
+            <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+
+            <!-- Scripts -->
+            <script src="{{ asset('public/js/app.js') }}" defer></script>
+        @else
+
+            <!-- Fonts -->
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+            <!-- Styles -->
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+            <!-- Scripts -->
+            <script src="{{ asset('js/app.js') }}" defer></script>
+        @endif
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
